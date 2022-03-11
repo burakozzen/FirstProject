f8fee44c4c0327c35bb0553f525e6ba254edb954
Burak ÖZEN <burakozen91@gmail.com>
2022-03-11 23:56:44 +0300

    Initial Commit

---
+++ FirstProject/FirstProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstProjectApp.swift
+//  FirstProject
+//
+//  Created by Burak ÖZEN on 11.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct FirstProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
